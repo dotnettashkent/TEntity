e269ea7ae63f24e68752aa4284cbb3d8ee43d2e0
Saeed <dotnettashkent@gmail.com>
2023-05-10 14:40:11 +0500

    Added Dtos

--- TEntity.Service/DTOs/Users/UserCreationDto.cs
+++ TEntity.Service/DTOs/Users/UserCreationDto.cs
@@ -2,5 +2,10 @@
 {
     public class UserCreationDto
     {
+        public string PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
